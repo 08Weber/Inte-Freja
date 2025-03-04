b904e266b978f0f6de816462e1cba634f89ec61d
weber <103030872+08Weber@users.noreply.github.com>
2025-03-04 13:35:08 +0100

    Update checklogin.js

--- js/checklogin.js
+++ js/checklogin.js
@@ -1,8 +1,6 @@
-import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
 
-// Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration  
 const firebaseConfig = {
   apiKey: "AIzaSyCTSoplo8zZHOVvFMQ0J9IarbkJUHRtuBs",
   authDomain: "freja-auth.firebaseapp.com",
@@ -14,59 +12,11 @@ const firebaseConfig = {
   measurementId: "G-YKK6L845XQ"
 };
 
-// Initialize Firebase
+// Initialize Firebase  
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
-// Retrieve the stored keyId from localStorage
-const keyId = localStorage.getItem("keyId");
+// Automatically log in the user  
+localStorage.setItem("isLoggedIn", "true");
 
-if (keyId) {
-  const keyDocRef = doc(db, "activationKeys", keyId);
-
-  // Check the user's key status on page load
-  if (keyId && !window.location.pathname.includes("välja.html")) {
-    getDoc(keyDocRef)
-      .then((docSnapshot) => {
-        if (docSnapshot.exists()) {
-          const data = docSnapshot.data();
-          
-          // Check if the key has expired
-          const expirationDate = data.expiresAt?.toDate();
-          if (expirationDate && expirationDate < new Date()) {
-            // Handle key expiration and log the user out
-            localStorage.removeItem("isLoggedIn");
-            localStorage.removeItem("keyId");
-            alert("Din licensekey har löpt ut och du kommer loggas ut.");
-            window.location.href = "./login.html";
-          }
-          
-          // Check if the key is not logged in
-          if (!data.isLoggedIn) {
-            // Handle user logout
-            localStorage.removeItem("isLoggedIn");
-            localStorage.removeItem("keyId");
-            alert("Your session has been deactivated. You have been logged out.");
-            window.location.href = "./login.html";
-          }
-        } else {
-          // Redirect inactive users
-          localStorage.removeItem("isLoggedIn");
-          localStorage.removeItem("keyId");
-          window.location.href = "./login.html";
-        }
-      })
-      .catch((error) => {
-        console.error("Error checking status:", error);
-        localStorage.removeItem("isLoggedIn");
-        localStorage.removeItem("keyId");
-        window.location.href = "./login.html";
-      });
-  } else if (!keyId) {
-    // If no keyId in localStorage, redirect to the login page
-    window.location.href = "./login.html";
-  }
-} else {
-  // If no keyId in localStorage, redirect to the login page
-  window.location.href = "./login.html";
-}
+// Redirect to the main page  
+window.location.href = "./dashboard.html";
